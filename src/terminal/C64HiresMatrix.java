6ce4866863c0c435043a255b288f849e7c81154d
pittbull <pbull809@gmail.com>
2017-01-25 20:56:48 +0100

    hires-painting with ARGB bitmap

--- src/terminal/C64HiresMatrix.java
+++ src/terminal/C64HiresMatrix.java
@@ -11,44 +11,53 @@ public class C64HiresMatrix
 {
     private BufferedImage bwImage;
     private final DataBuffer db;
-    private final byte[] rawbytes = new byte[25 * 40 * 8 * 8];
+    private final int[] rawints = new int[26 * 40 * 8 * 8];
     private final int offset = 8192;
 
     public C64HiresMatrix()
     {
-        bwImage = new BufferedImage (320, 200, BufferedImage.TYPE_BYTE_BINARY);
-        //Image img = BitmapTools.makeColorTransparent(bwImage, new Color(0,0,0));
-        //bwImage = BitmapTools.imageToBufferedImage(img, BufferedImage.TYPE_BYTE_BINARY);
+        bwImage = new BufferedImage (320, 200, BufferedImage.TYPE_INT_ARGB);
         db = bwImage.getRaster().getDataBuffer();
     }
 
-    public byte peek (int addr)
+    public int peek (int addr)
     {
-        return rawbytes[addr-offset];
+        addr = (addr-offset)*8;
+        return 0; //rawints[addr-offset];
+        // TODO: fill this with usable code
     }
 
-    public void poke (int addr, byte val)
+    public void poke (int addr, int val)
     {
-        rawbytes[addr-offset] = val;
+        addr = (addr-offset)*8;
+        int b = 128;
+        while(b != 0)
+        {
+            if ((val & b) == b)
+                rawints[addr] = Color.RED.getRGB();
+            else
+                rawints[addr] = 0;
+            addr++;
+            b>>>=1;
+        }
     }
 
     public void render (Graphics g)
     {
         int counter = 0;
-        for (int row = 0; row < 8000; row+=320)
+        for (int r = 0; r < 64000; r+=2560)
         {
-            for (int cc = 0; cc < 40; cc++)
+            for (int c = 0; c < 320; c+=8)
             {
-                for (int s = 0; s < 320; s+=40)
+                for (int s = 0; s < 2560; s+=320)
                 {
-                    db.setElem(s + row + cc, rawbytes[counter++]);
+                    for (int in = 0; in < 8; in++)
+                    {
+                        db.setElem(c + in + r + s, rawints[counter++]);
+                    }
                 }
             }
         }
-//        g.setPaintMode();
-//        g.setXORMode(Color.WHITE);
-//        g.setColor(Color.RED);
-//        g.fillRect(10,10,300,300);
         g.drawImage(bwImage, 0, 0, 2 * 320, 2 * 200, null);
     }
 }
